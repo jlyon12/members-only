aafe8c1d598a13a9ce67a389d121f5ed98b4321b
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-10-06 13:26:18 -0600

    create index page

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 connectDB();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views', 'pages'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
